efd70c94a8fd15736ca601ab2fe89c79e3f6d4af
LenaGmbh <jenichenfabian617@gmail.com>
2022-08-24 03:06:58 +0200

    Initial Commit

--- src/MainSite.js
+++ src/MainSite.js
@@ -9,7 +9,6 @@ function MainSite() {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
                       integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
                       crossOrigin="anonymous"/>
-                livechat()
             </header>
             <body>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
